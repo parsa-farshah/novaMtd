f8056d3a9e80ca9284dc0593beb8b493a28ca795
parsa <parsafarashah@gmail.com>
2026-06-09 18:55:54 +0330

    add why choose us

---
+++ app/components/WhyChooseUs.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { FaCheckSquare } from "react-icons/fa";
+
+const WhyChooseUs = () => {
+  return (
+    <section className="mt-[69px] px-6 flex flex-col h-fit w-fit items-center lg:flex-row lg:items-center  lg:gap-6">
+      <div className="lg:w-[113px] flex items-center h-fit">
+        <span className="font-vazir text-2xl font-bold lg:text-center  lg:text-[28px]  ">
+          مزایای همکاری با novamtd
+        </span>
+      </div>
+      {/* cards */}
+      <section className="flex flex-col gap-2 mt-[58px] md:mt-[36px] md:flex-row md:justify-between md:px-6 md:gap-0">
+        {/* card  */}
+        <section className="w-[280px] md:w-[32%]">
+          <div className="bg-primary rounded-3xl px-3 py-6 flex items-center gap-6">
+            <FaCheckSquare className="text-white text-5xl" />
+            <div className="flex flex-col gap-5">
+              <span className="font-vazir text-xs font-bold text-white lg:text-sm">
+                لورم ایپسوم متن ساختگی تولید
+              </span>
+              <p className="font-vazir text-[8px] text-[#E3E3E3] leading-4 lg:text-xs lg:w-[70%]">
+                لورم ایپسوم متن ساختگی با تولیدلورم ایپسوم متن ساختگی با تولید
+                لورم ایپسوم متن ساختگی با تولید
+              </p>
+            </div>
+          </div>
+        </section>
+        {/* card 2 */}
+        <section className="w-[280px] md:w-[32%]">
+          <div className="bg-primary rounded-3xl px-3 py-6 flex items-center gap-6">
+            <FaCheckSquare className="text-white text-5xl" />
+            <div className="flex flex-col gap-5">
+              <span className="font-vazir text-xs font-bold text-white lg:text-sm">
+                لورم ایپسوم متن ساختگی تولید
+              </span>
+              <p className="font-vazir text-[8px] text-[#E3E3E3] leading-4 lg:text-xs lg:w-[70%]">
+                لورم ایپسوم متن ساختگی با تولیدلورم ایپسوم متن ساختگی با تولید
+                لورم ایپسوم متن ساختگی با تولید
+              </p>
+            </div>
+          </div>
+        </section>{" "}
+        {/* card 3 */}
+        <section className="w-[280px] md:w-[32%]">
+          <div className="bg-primary rounded-3xl px-3 py-6 flex items-center gap-6">
+            <FaCheckSquare className="text-white text-5xl" />
+            <div className="flex flex-col gap-5">
+              <span className="font-vazir text-xs font-bold text-white lg:text-sm">
+                لورم ایپسوم متن ساختگی تولید
+              </span>
+              <p className="font-vazir text-[8px] text-[#E3E3E3] leading-4 lg:text-xs lg:w-[70%]">
+                لورم ایپسوم متن ساختگی با تولیدلورم ایپسوم متن ساختگی با تولید
+                لورم ایپسوم متن ساختگی با تولید
+              </p>
+            </div>
+          </div>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default WhyChooseUs;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Top from "./components/Top";
 import OurServices from "./components/OurServices";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Top />
       <Header />
       <OurServices />
+      <WhyChooseUs />
     </div>
   );
 }
