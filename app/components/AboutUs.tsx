026f09140af8cad9342fb3a5bb77b8fcf8c47031
parsa <parsafarashah@gmail.com>
2026-06-23 16:24:37 +0330

    change image about

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -8,12 +8,7 @@ const AboutUs = () => {
       <AboutUsTxt />
       <div className="w-full h-[280px] px-6 md:h-[298px] lg:w-[40%]">
         <figure className="relative w-full h-full rounded-[50px] overflow-hidden">
-          <Image
-            src="/images/about.jfif"
-            alt="aboutUs"
-            fill
-            objectFit="cover"
-          />
+          <Image src="/images/about.jpg" alt="aboutUs" fill objectFit="cover" />
         </figure>
       </div>
     </div>
