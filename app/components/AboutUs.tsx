0fc0a0e973e0d47f9a2509bf64d61417b5a5fd9c
parsa <parsafarashah@gmail.com>
2026-06-23 13:33:09 +0330

    space beetween section on home

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const AboutUs = () => {
   return (
-    <div className="flex flex-col gap-9 md:flex-row-reverse md:items-center md:justify-center md:gap-0 px-6 py-3 md:px-9 overflow-hidden">
+    <div className="flex flex-col gap-9 md:flex-row-reverse md:items-center md:justify-center md:gap-0 px-6 py-3 md:px-9 overflow-hidden mt-60 md:mt-146 lg:mt-60">
       <AboutUsTxt />
       <div className="w-full h-[280px] px-6 md:h-[298px] lg:w-[40%]">
         <figure className="relative w-full h-full rounded-[50px] overflow-hidden">
